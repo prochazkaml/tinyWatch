aa921eb403830144b5e6913bc4730a36dd2a398f
Michal Procházka <prochazka2003@gmail.com>
2022-06-26 20:07:28 +0200

    Made the build script easier to use (no need to pass any params anymore)

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-[ $# -ne 2 ] && echo "Usage: $0 projectname mcu" && exit 1
+avr-gcc -Os -mmcu=attiny1614 -c -o main.o main.c
+avr-gcc -Os -mmcu=attiny1614 -o main.elf crtattiny1614.o main.o -Wl,--relax,--gc-sections -nostartfiles
 
-avr-gcc -Os -mmcu=attiny1614 -c -o $1.o $1.c
-avr-gcc -Os -mmcu=attiny1614 -o $1.elf crtattiny1614.o $1.o -Wl,--relax,--gc-sections -nostartfiles
-#avr-objcopy -j .text -j .data -O binary $1.elf $1.bin
-#sudo avrdude -U flash:w:$1.elf:e -c usbasp -p $2
 if [ -f main.elf ]; then
-	avr-size $1.elf
-	avrdude -C avrdude.conf -c jtag2updi -p $2 -b 115200 -P /dev/ttyUSB0 -U flash:w:$1.elf:e
+	avr-size main.elf
+	avrdude -C avrdude.conf -c jtag2updi -p attiny1614 -b 115200 -P /dev/ttyUSB0 -U flash:w:main.elf:e
 else
 	echo "Sorry, but something went horribly wrong."
 fi
